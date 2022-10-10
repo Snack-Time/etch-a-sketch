d2a30fb59fa0cff6cf762118da0497aaa24e39aa
Snack-Time <wmcnally1226@gmail.com>
2022-10-10 17:10:43 -0400

    make the grid fill in

--- script.js
+++ script.js
@@ -7,7 +7,12 @@ let gridDimensions = prompt("Enter a number stoopid", "1");
 gridMaker(gridDimensions);
 
 function gridMaker(num) {
-    for (let i = 0; i < num; i++) {
+    gridContainerSelector.style.gridTemplateColumns = `repeat(${gridDimensions}, 1fr)`;
+    gridContainerSelector.style.gridTemplateRows = `repeat(${gridDimensions}, 1fr)`;
+
+    let gridArea = num * num;
+
+    for (let i = 0; i < gridArea; i++) {
         const div = document.createElement("div");
         div.classList.add("gridSquare");
         gridContainerSelector.appendChild(div);
